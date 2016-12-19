ec3dd85fcff14532bd142856be3d8e89ffc80e13
mitsuru1397 <g1527068@st.t-kougei.ac.jp>
2016-12-19 16:17:45 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// BASIC 認証
+var basicAuth = rewuire('basic-auth-connect');
+app.use(basicAuth('hoge', 'hogepass'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
